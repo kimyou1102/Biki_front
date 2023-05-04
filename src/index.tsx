c69bbd5d3df6eaad5bb52659b404bc7bd7613caf
kimyou1102 <yoosk5485@naver.com>
2023-05-04 21:08:12 +0900

    setting: 초기 세팅
    - router, recoil, styled-components, styled-reset, GlobalStyle
    - 폴더 생성

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from './styles/globalStyle';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RecoilRoot>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </RecoilRoot>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
