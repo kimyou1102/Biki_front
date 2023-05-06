a742bea860b9a321b583a8f44f9e5e6a6e2d2dc0
kimyou1102 <yoosk5485@naver.com>
2023-05-06 23:41:18 +0900

    style: 전역 스타일 설정

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,8 +1,30 @@
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import PretendardBold from '../assets/fonts/Pretendard-Bold.woff2';
+import PretendardMedium from '../assets/fonts/Pretendard-Medium.woff2';
+import PretendardRegular from '../assets/fonts/Pretendard-Regular.woff2';
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
+    *, body {
+        font-family: 'PretendardRegular'
+    }
+
+    @font-face {
+        font-family: 'PretendardBold';
+        src: local('PretendardBold'), url(${PretendardBold}) format('woff2');
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'PretendardMedium';
+        src: local('PretendardMedium'), url(${PretendardMedium}) format('woff2');
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'PretendardRegular';
+        src: local('PretendardRegular'), url(${PretendardRegular}) format('woff2');
+        font-style: normal;
+    }
 `;
 
 export default GlobalStyle;
