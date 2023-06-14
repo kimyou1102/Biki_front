7248f27209d8dbe6644261ae731dc398beb86774
kimyou1102 <yoosk5485@naver.com>
2023-06-14 20:56:42 +0900

    feat: 메인페이지 배경 아이콘 추가

--- src/components/organisms/FooterBannerList.tsx
+++ src/components/organisms/FooterBannerList.tsx
@@ -4,41 +4,64 @@ import { Button, Img } from '@atoms';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import left from '../../assets/images/leftSlide.png';
-import right from '../../assets/images/rightSlide.png';
-
-const test = Array.from({ length: 8 }, (v, i) => i + 1);
-
-const Container = styled.div`
-  width: calc(1520px * 0.8);
-  margin: 0 auto;
-`;
-
-const Banner = styled.li`
-  display: inline-block;
-`;
+import left from '../../assets/images/banner_left.png';
+import right from '../../assets/images/banner_right.png';
 
 interface ArrowProps {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
+const Container = styled.div`
+  width: calc(1520px * 0.8);
+  height: calc(60px * 0.8);
+  margin: 0 auto;
+`;
+
+const StyledButton = styled.button`
+  position: absolute;
+  width: calc(40px * 0.8);
+  height: calc(40px * 0.8);
+  background: white;
+  box-shadow: 0px 0px 28px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0px;
+  border: none;
+  border-radius: 50%;
+  z-index: 10;
+  cursor: pointer;
+
+  &.left {
+    left: 0px;
+    transform: translate(-50%, 0%);
+  }
+
+  &.right {
+    right: 0px;
+    transform: translate(50%, 0%);
+  }
+`;
+
 function PrevArrow({ onClick }: ArrowProps) {
   return (
-    <Button onClick={onClick} className="left">
-      <Img src={left} alt="왼쪽화살표" width={38} height={67} />
-    </Button>
+    <StyledButton onClick={onClick} className="left">
+      <Img src={left} alt="왼쪽화살표" width={8} height={14} />
+    </StyledButton>
   );
 }
 
 function NextArrow({ onClick }: ArrowProps) {
   return (
-    <Button onClick={onClick} className="right">
-      <Img src={right} alt="오른쪽화살표" width={38} height={67} />
-    </Button>
+    <StyledButton onClick={onClick} className="right">
+      <Img src={right} alt="오른쪽화살표" width={8} height={14} />
+    </StyledButton>
   );
 }
 
 export function FooterBannerList() {
+  const test = Array.from({ length: 8 }, (v, i) => i + 1);
+
   const settings = {
     loop: true,
     infinite: true,
@@ -56,11 +79,11 @@ export function FooterBannerList() {
       <Slider {...settings}>
         {test.map((e) => (
           <ul key={e}>
-            <Banner>
-              <Button width={200} bgcolor="tomato">
+            <li>
+              <Button width={200} height={40} bgcolor="tomato">
                 후원사배너{e}
               </Button>
-            </Banner>
+            </li>
           </ul>
         ))}
       </Slider>
