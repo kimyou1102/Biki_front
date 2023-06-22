7e3c72b2aa22ac29875f2a3d792164cb95fd51ca
soyuly <soyul8147@gmail.com>
2023-06-22 20:03:06 +0900

    fix: 영상 시간 고정 문제 해결 및 배너 스와이퍼 복구

--- src/components/pages/movieSchedule/index.tsx
+++ src/components/pages/movieSchedule/index.tsx
@@ -78,7 +78,6 @@ export function ScheduleInfoPage() {
       getMoviesByVenueAndDate(venueId, currentDate!)
         .then((res) => {
           setMovieSchedule(res.data.data);
-          setCurrentDate(res.data.data.runningDateStart);
 
           // runningDateStart 부터 runningDateEnd까지 날짜를 전부 저장하는 배열 만들기 위한 변수
           const tempDates = [];
@@ -93,6 +92,10 @@ export function ScheduleInfoPage() {
             tempDates.push(`${year}-${month}-${day}`); // 2023-07-11 형태로 저장
 
             startDate.setDate(startDate.getDate() + 1);
+          }
+
+          if (!tempDates.includes(currentDate)) {
+            setCurrentDate(res.data.data.runningDateStart);
           }
 
           // 만들어진 날짜 배열을 저장
