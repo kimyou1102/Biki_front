4bd2efdbf71b87b139dad4c514daf5f719007d03
soyuly <soyul8147@gmail.com>
2023-06-23 13:24:25 +0900

    fix: 머지 아카이브 모달 오류 수정

--- src/components/molecules/MainNewsBoxText.tsx
+++ src/components/molecules/MainNewsBoxText.tsx
@@ -11,13 +11,14 @@ interface Props {
 
 export function MainNewsBoxText({ date, title, id, url }: Props) {
   const navigate = useNavigate();
+  console.log('타이틀', title);
   return (
     <Li outline="#767676" padding="16px 0 16px 0" onClick={() => navigate(`${url}/${id}`)}>
       <FlexContainer>
         <Span weight="lighter" size={0.5} margin="0 calc(40px * 0.8) 0 0">
           {date.replace(/\//gi, '.').substring(2)}
         </Span>
-        <Span weight="bold">{title.length > 42 ? `${title.slice(0, 42)}...` : title}</Span>
+        <Span weight="bold">{title && title.length > 42 ? `${title.slice(0, 42)}...` : title}</Span>
       </FlexContainer>
     </Li>
   );
